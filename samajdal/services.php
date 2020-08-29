09ee3d9288332966799609be5ab160cb2c2d725f
bhumimpatel <70433534+bhumimpatel@users.noreply.github.com>
2020-08-29 16:11:25 +0530

    publishing my 1st wedsite
    wow i am very happy :)

---
+++ samajdal/services.php
@@ -0,0 +1,355 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Samaj Dal</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.php">About Us</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="services.php">Services</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+
+    <section class="my-5">
+        <div class="py-5">
+             <h3 class="text-center">Our Services</h3>
+        </div>
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-4 col-md-4 col-12">
+              <div class="card">
+                <img class="card-img-top" src="day1.jpg" alt="Card image">
+                <div class="card-body">
+                   <h4 class="card-title">Day 1</h4>
+                    <p class="card-text">COVID-19 days our proived services  </p>
+                    <a href="seemore.php" class="btn btn-primary">See Profile</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+              <div class="card">
+                <img class="card-img-top" src="day2.jpg" alt="Card image">
+                <div class="card-body">
+                   <h4 class="card-title">Day 2</h4>
+                    <p class="card-text">COVID-19 days our proived services </p>
+                    <a href="services.php" class="btn btn-primary">See Profile</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+              <div class="card">
+                <img class="card-img-top" src="day3.jpg" alt="Card image">
+                <div class="card-body">
+                   <h4 class="card-title">Day 3</h4>
+                    <p class="card-text">COVID-19 days our proived services  </p>
+                    <a href="services.php" class="btn btn-primary">See Profile</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+              <div class="card">
+                <img class="card-img-top" src="day4.jpg" alt="Card image">
+                <div class="card-body">
+                   <h4 class="card-title">Day 4</h4>
+                    <p class="card-text">COVID-19 days our proived services </p>
+                    <a href="services.php" class="btn btn-primary">See Profile</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+              <div class="card">
+                <img class="card-img-top" src="day5.jpg" alt="Card image">
+                <div class="card-body">
+                   <h4 class="card-title">Day 5</h4>
+                    <p class="card-text">COVID-19 days our proived services </p>
+                    <a href="services.php" class="btn btn-primary">See Profile</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+              <div class="card">
+                <img class="card-img-top" src="day6.jpg" alt="Card image">
+                <div class="card-body">
+                   <h4 class="card-title">Day 6</h4>
+                    <p class="card-text">COVID-19 days our proived services </p>
+                    <a href="services.php" class="btn btn-primary">See Profile</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+              <div class="card">
+                <img class="card-img-top" src="day7.jpg" alt="Card image">
+                <div class="card-body">
+                   <h4 class="card-title">Day 7</h4>
+                    <p class="card-text">COVID-19 days our proived services </p>
+                    <a href="services.php" class="btn btn-primary">See Profile</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+              <div class="card">
+                <img class="card-img-top" src="day8.jpg" alt="Card image">
+                <div class="card-body">
+                   <h4 class="card-title">Day 8</h4>
+                    <p class="card-text">COVID-19 days our proived services </p>
+                    <a href="services.php" class="btn btn-primary">See Profile</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+              <div class="card">
+                <img class="card-img-top" src="day9.jpg" alt="Card image">
+                <div class="card-body">
+                   <h4 class="card-title">Day 9</h4>
+                    <p class="card-text">COVID-19 days our proived services </p>
+                    <a href="services.php" class="btn btn-primary">See Profile</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+              <div class="card">
+                <img class="card-img-top" src="day10.jpg" alt="Card image">
+                <div class="card-body">
+                   <h4 class="card-title">Day 10</h4>
+                    <p class="card-text">COVID-19 days our proived services </p>
+                    <a href="services.php" class="btn btn-primary">See Profile</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+              <div class="card">
+                <img class="card-img-top" src="day11.jpg" alt="Card image">
+                <div class="card-body">
+                   <h4 class="card-title">Day 11</h4>
+                    <p class="card-text">COVID-19 days our proived services </p>
+                    <a href="services.php" class="btn btn-primary">See Profile</a>
+                </div>
+              </div>
+            </div>
+            
+            <div class="col-lg-4 col-md-4 col-12">
+              <div class="card">
+                <img class="card-img-top" src="day12.jpg" alt="Card image">
+                <div class="card-body">
+                   <h4 class="card-title">Day 12</h4>
+                    <p class="card-text">COVID-19 days our proived services </p>
+                    <a href="services.php" class="btn btn-primary">See Profile</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+              <div class="card">
+                <img class="card-img-top" src="day13.jpg" alt="Card image">
+                <div class="card-body">
+                   <h4 class="card-title">Day 13</h4>
+                    <p class="card-text">COVID-19 days our proived services </p>
+                    <a href="services.php" class="btn btn-primary">See Profile</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+              <div class="card">
+                <img class="card-img-top" src="day14.jpg" alt="Card image">
+                <div class="card-body">
+                   <h4 class="card-title">Day 14</h4>
+                    <p class="card-text">COVID-19 days our proived services  </p>
+                    <a href="services.php" class="btn btn-primary">See Profile</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+              <div class="card">
+                <img class="card-img-top" src="day17.jpg" alt="Card image">
+                <div class="card-body">
+                   <h4 class="card-title">Day17</h4>
+                    <p class="card-text">COVID-19 days our proived services </p>
+                    <a href="services.php" class="btn btn-primary">See Profile</a>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-lg-4 col-md-4 col-12">
+              <div class="card">
+                <img class="card-img-top" src="day18.jpg" alt="Card image">
+                <div class="card-body">
+                   <h4 class="card-title">Day18</h4>
+                    <p class="card-text">COVID-19 days our proived services </p>
+                    <a href="services.php" class="btn btn-primary">See Profile</a>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-lg-4 col-md-4 col-12">
+              <div class="card">
+                <img class="card-img-top" src="day.jpg" alt="Card image">
+                <div class="card-body">
+                   <h4 class="card-title">End of Day</h4>
+                    <p class="card-text">COVID-19 days our proived services </p>
+                    <a href="services.php" class="btn btn-primary">See Profile</a>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+         <section class="my-5">
+        <div class="py-5">
+             <h4 class="text-center">Next Service </h4>
+        </div>
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-4 col-md-4 col-12">
+              <div class="card">
+                <img class="card-img-top" src="sam1.jpg" alt="Card image">
+                <div class="card-body">
+                   <h4 class="card-title">Be safe</h4>
+                    <p class="card-text">COVID-19 days our proived Services.... :) </p>
+                    <a href="services.php" class="btn btn-primary">See Profile</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+              <div class="card">
+                <img class="card-img-top" src="sam2.jpg" alt="Card image">
+                <div class="card-body">
+                   <h4 class="card-title">Be safe</h4>
+                    <p class="card-text">COVID-19 days our proived Services.... :) </p>
+                    <a href="services.php" class="btn btn-primary">See Profile</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+              <div class="card">
+                <img class="card-img-top" src="sam3.jpg" alt="Card image">
+                <div class="card-body">
+                   <h4 class="card-title">Be safe</h4>
+                    <p class="card-text">COVID-19 days our proived Services.... :) </p>
+                    <a href="services.php" class="btn btn-primary">See Profile</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+              <div class="card">
+                <img class="card-img-top" src="sam.jpg" alt="Card image">
+                <div class="card-body">
+                   <h4 class="card-title">Be safe</h4>
+                    <p class="card-text">COVID-19 days our proived Services.... :) </p>
+                    <a href="services.php" class="btn btn-primary">See Profile</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <section class="my-5">
+        <div class="py-5">
+             <h4 class="text-center">Next Service </h4>
+        </div>
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-4 col-md-4 col-12">
+              <div class="card">
+                <img class="card-img-top" src="1samajdal.jpg" alt="Card image">
+                <div class="card-body">
+                   <h4 class="card-title">Be safe</h4>
+                    <p class="card-text">COVID-19 days our proived Mask.... :) </p>
+                    <a href="services.php" class="btn btn-primary">See Profile</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+              <div class="card">
+                <img class="card-img-top" src="4samajdal.jpg" alt="Card image">
+                <div class="card-body">
+                   <h4 class="card-title">Be safe</h4>
+                    <p class="card-text">COVID-19 days our proived Mask.... :) </p>
+                    <a href="services.php" class="btn btn-primary">See Profile</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+              <div class="card">
+                <img class="card-img-top" src="02samajdal.jpg" alt="Card image">
+                <div class="card-body">
+                   <h4 class="card-title">Be safe</h4>
+                    <p class="card-text">COVID-19 days our proived Mask.... :) </p>
+                    <a href="services.php" class="btn btn-primary">See Profile</a>
+                </div>
+              </div>
+            </div>
+             <div class="col-lg-4 col-md-4 col-12">
+              <div class="card">
+                <img class="card-img-top" src="6samajdal.jpg" alt="Card image">
+                <div class="card-body">
+                   <h4 class="card-title">Be safe</h4>
+                    <p class="card-text">COVID-19 days our proived Mask.... :) </p>
+                    <a href="services.php" class="btn btn-primary">See Profile</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+              <div class="card">
+                <img class="card-img-top" src="5samajdal.jpg" alt="Card image">
+                <div class="card-body">
+                   <h4 class="card-title">Be safe</h4>
+                    <p class="card-text">COVID-19 days our proived Mask.... :) </p>
+                    <a href="services.php" class="btn btn-primary">See Profile</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-12">
+              <div class="card">
+                <img class="card-img-top" src="2samajdal.jpg" alt="Card image">
+                <div class="card-body">
+                   <h4 class="card-title">Be safe</h4>
+                    <p class="card-text">COVID-19 days our proived Mask.... :) </p>
+                    <a href="services.php" class="btn btn-primary">See Profile</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        </section>
+        
+        <footer>
+            <div>
+              <p class="p-3 bg-dark text-white text-center">Samaj Dal</p>
+            </div>
+        </footer>
+        
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</body>
+</html>
